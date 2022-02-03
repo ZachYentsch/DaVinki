963ab112f21fde26c86f5f061a1d9e94a86cd27e
KarinnaGorrono <karinna.gorrono@gmail.com>
2022-02-03 09:57:59 -0700

    commit

---
+++ DaVinki.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-b7ths2an.us.auth0.com'
+export const clientId = 'a7hOBqh6Q6GK1tWNtLywxoNBJSAEVzty'
+export const audience = 'https://DaVinki.com'
