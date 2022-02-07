f9f82f98e8d0bc293026df003cd46570f71f0c86
Zach <z6chy@icloud.com>
2022-02-07 15:07:58 -0700

    spun up server

--- DaVinki/client/app/env.js
+++ DaVinki/client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-b7ths2an.us.auth0.com'
+export const audience = 'https://DaVinki.com'
+export const clientId = 'glXQ2lBOMkhnGGdXTJNchjGebJJK9yvt'
